7e74020093328c688b96c642e090bd2be054c34a
YASH GUPTA <guptayash2804@gmail.com>
2026-01-25 16:57:14 +0530

    Time: 358 ms (32.24%) | Memory: 289.7 MB (29.39%) - LeetSync

--- 2389-design-a-text-editor/design-a-text-editor.cpp
+++ 2389-design-a-text-editor/design-a-text-editor.cpp
@@ -1,37 +1,83 @@
 class TextEditor {
 public:
-    string left;   
-    string right;  
+    struct Node {
+        char c;
+        Node* prev;
+        Node* next;
+        Node(char ch = '#') : c(ch), prev(nullptr), next(nullptr) {}
+    };
+
+    Node* head;   
+    Node* tail;   
+    Node* cursor; 
 
     TextEditor() {
-        left = "";
-        right = "";
+        head = new Node();
+        tail = new Node();
+        head->next = tail;
+        tail->prev = head;
+        cursor = head; 
     }
     
     void addText(string text) {
-        left += text;
+        for (char ch : text) {
+            Node* node = new Node(ch);
+            Node* right = cursor->next;
+            node->prev = cursor;
+            node->next = right;
+            cursor->next = node;
+            right->prev = node;
+            cursor = node; 
+        }
     }
     
     int deleteText(int k) {
-        int del = min(k, (int)left.size());
-        left.erase(left.size() - del, del);
-        return del;
+        int deleted = 0;
+        while (cursor != head && k > 0) {
+            Node* toDelete = cursor;
+            Node* left = toDelete->prev;
+            Node* right = toDelete->next;
+            left->next = right;
+            right->prev = left;
+            cursor = left;
+            delete toDelete;
+            deleted++;
+            k--;
+        }
+        return deleted;
     }
     
     string cursorLeft(int k) {
-        int move = min(k, (int)left.size());
-        right = left.substr(left.size() - move, move) + right;
-        left.erase(left.size() - move, move);
-        return left.substr(left.size() > 10 ? left.size() - 10 : 0);
+        while (cursor != head && k > 0) {
+            cursor = cursor->prev;
+            k--;
+        }
+        return getLeft10();
     }
     
     string cursorRight(int k) {
-        int move = min(k, (int)right.size());
-        left += right.substr(0, move);
-        right.erase(0, move);
-        return left.substr(left.size() > 10 ? left.size() - 10 : 0);
+        while (cursor->next != tail && k > 0) {
+        cursor = cursor->next;
+        k--;
+        }
+        return getLeft10();
+    }
+
+private:
+    string getLeft10() {
+        string res;
+        Node* p = cursor;
+        int cnt = 0;
+        while (p != head && cnt < 10) {
+            res.push_back(p->c);
+            p = p->prev;
+            cnt++;
+        }
+        reverse(res.begin(), res.end());
+        return res;
     }
 };
+
 
 /**
  * Your TextEditor object will be instantiated and called as such:
