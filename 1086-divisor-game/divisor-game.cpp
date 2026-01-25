c97d79757b77ac162210ab4b39c812ad15e808ec
YASH GUPTA <guptayash2804@gmail.com>
2026-01-25 15:10:23 +0530

    Time: 3 ms (9.30%) | Memory: 8.2 MB (12.29%) - LeetSync

---
+++ 1086-divisor-game/divisor-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool divisorGame(int n) {
+        vector<bool> dp(n + 1, false);
+        dp[1] = false;  
+        for (int i = 2; i <= n; i++) {
+            for (int x = 1; x < i; x++) {
+                if (i % x == 0 && !dp[i - x]) {  
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+};
