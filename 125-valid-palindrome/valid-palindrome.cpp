72305ab4c4697fb605ffc40883591097e7730ab9
YASH GUPTA <guptayash2804@gmail.com>
2026-02-18 16:23:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (77.30%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -3,12 +3,8 @@ public:
     bool isPalindrome(string s) {
         int left = 0, right = s.size() - 1;
         while (left < right) {
-            while (left < right && !((s[left] >= 'a' && s[left] <= 'z') || 
-                                     (s[left] >= 'A' && s[left] <= 'Z') || 
-                                     (s[left] >= '0' && s[left] <= '9'))) ++left;
-            while (left < right && !((s[right] >= 'a' && s[right] <= 'z') || 
-                                     (s[right] >= 'A' && s[right] <= 'Z') || 
-                                     (s[right] >= '0' && s[right] <= '9'))) --right;
+            while (left < right && !isalnum(s[left])) ++left;
+            while (left < right && !isalnum(s[right])) --right;
             if (tolower(s[left++]) != tolower(s[right--])) return false;
         }
         return true;
